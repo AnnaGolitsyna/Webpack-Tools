a0229303e2e6df480ede07f7aa2e894a80d434d6
AnnaGolitsyna <110576gav@gmail.com>
2022-12-30 16:54:58 +0200

    task1

---
+++ lesson3/task2TodoList/src/scripts/createTask.spec.js
@@ -0,0 +1,14 @@
+import { createNewTask } from './createTask.js';
+
+it('should new task', () => {
+
+    const text = 'task 6';
+    const done = false;
+    const time = 1672220432765;
+
+  expect(createNewTask()).toEqual({
+    text: 'task 6',
+    done: false,
+    time: 1672220432765,
+  });
+});
